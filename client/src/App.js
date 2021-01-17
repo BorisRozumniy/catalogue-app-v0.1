93a8ac7b70cf5ba2cb40a52b812cd31c47620ce6
dmitrylineup <kotelevsky.dima@gmail.com>
2021-01-17 14:23:43 +0200

    remove comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import 'react-toastify/dist/ReactToastify.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { useAuth } from './hooks/auth.hook';
-import { AuthContext } from "./context/AuthContext";
-import Routes from './routes/frontendRoutes';
+import Routes from "./routes/frontendRoutes";
 
 const App = () => {
-  // const { email, saveEmail, token, login, logout, userId } = useAuth();
-  // const isAuthenticated = !!token;
-
   return (
-    // <AuthContext.Provider value={{
-    //   email, token, login, logout, userId, isAuthenticated
-    // }}>
-      <BrowserRouter>
-        {/* {isAuthenticated && <Header />} */}
-        <Routes />
-      </BrowserRouter>
-    // </AuthContext.Provider>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
