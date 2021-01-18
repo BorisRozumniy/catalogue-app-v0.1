04b4c5c77767e2056af833376c114f64116b5414
dmitrylineup <kotelevsky.dima@gmail.com>
2021-01-18 22:05:43 +0200

    corrected logout button

--- client/src/modules/header/Header.js
+++ client/src/modules/header/Header.js
@@ -2,46 +2,45 @@ import React from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { Navbar, Nav, NavItem, Container } from "reactstrap";
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-// import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import {
+  Button,
+  Drawer,
+  AppBar,
+  Toolbar,
+  List,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemIcon,
+} from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import MenuIcon from '@material-ui/icons/Menu';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
+import { Home as HomeIcon, Menu as MenuIcon, MoveToInbox as InboxIcon} from "@material-ui/icons";
+// import InboxIcon from "@material-ui/icons/MoveToInbox";
 // import HomeIcon from '@material-ui/icons/HomeIcon';
-import HomeIcon from '@material-ui/icons/Home';
+// import HomeIcon from "@material-ui/icons/Home";
 
-import MailIcon from '@material-ui/icons/Mail';
+import MailIcon from "@material-ui/icons/Mail";
 import { frontendUrls } from "../../routes/frontendUrls";
 import { actionLogout } from "../../redux/actions/auth";
 import { actionClearProducts } from "../../redux/actions/products";
-
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
   },
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -51,7 +50,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
@@ -61,24 +60,24 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginRight: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -98,7 +97,7 @@ const Header = ({ email, actionLogout, actionClearProducts }) => {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-  
+
   const handleLogout = () => {
     actionClearProducts();
     actionLogout();
@@ -106,51 +105,58 @@ const Header = ({ email, actionLogout, actionClearProducts }) => {
   return (
     <Navbar color="light" light expand="md" className="mb-4">
       <Container className="justify-content-center">
-      <AppBar
-        position="fixed"
-        // className={clsx(classes.appBar, {
-        //   [classes.appBarShift]: open,
-        // })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="end"
-            onClick={handleDrawerOpen}
-            // className={clsx(open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Nav className="text-uppercase p-3">
-            <NavItem className="mr-2 my-4 my-lg-2">
-              <NavLink
-                exact
-                to={frontendUrls.home}
-                activeClassName="bg-warning"
-                className="p-3"
-              >
-                home
-              </NavLink>
-            </NavItem>
-            <NavItem className="mr-2 my-4 my-lg-2">
-              <NavLink
-                to={frontendUrls.catalogue}
-                activeClassName="bg-warning"
-                className="p-3"
-              >
-                catalogue
-              </NavLink>
-            </NavItem>
-            <div className="mr-2 my-4 my-lg-2">
-              {email && email}
-              <button type="button rounded" onClick={handleLogout}>
-                logout
-              </button>
-            </div>
-          </Nav>
-        </Toolbar>
-      </AppBar>
+        <AppBar
+          position="fixed"
+          // className={clsx(classes.appBar, {
+          //   [classes.appBarShift]: open,
+          // })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="end"
+              onClick={handleDrawerOpen}
+              // className={clsx(open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Nav className="text-uppercase p-3">
+              <NavItem className="mr-2 my-4 my-lg-2">
+                <NavLink
+                  exact
+                  to={frontendUrls.home}
+                  activeClassName="bg-warning"
+                  className="p-3"
+                >
+                  home
+                </NavLink>
+              </NavItem>
+              <NavItem className="mr-2 my-4 my-lg-2">
+                <NavLink
+                  to={frontendUrls.catalogue}
+                  activeClassName="bg-warning"
+                  className="p-3"
+                >
+                  catalogue
+                </NavLink>
+              </NavItem>
+              <div className="mr-2 my-4 my-lg-2">
+                {email && email}
+                <Button
+                  onClick={handleLogout}
+                  color="white"
+                  variant="contained"
+                  color="default"
+                  className={classes.button}
+                  style={{ marginLeft: 8 }}
+                >
+                  logout
+                </Button>
+              </div>
+            </Nav>
+          </Toolbar>
+        </AppBar>
       </Container>
       <Drawer
         className={classes.drawer}
@@ -163,30 +169,28 @@ const Header = ({ email, actionLogout, actionClearProducts }) => {
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            {theme.direction === "rtl" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
           </IconButton>
         </div>
         <Divider />
         <List>
-          <ListItem button >
+          <ListItem button>
             <ListItemIcon>
               <InboxIcon />
             </ListItemIcon>
-            <NavLink
-              to={frontendUrls.catalogue}
-              activeClassName="border"
-            >
+            <NavLink to={frontendUrls.catalogue} activeClassName="border">
               catalogue
             </NavLink>
           </ListItem>
-          <ListItem button >
+          <ListItem button>
             <ListItemIcon>
               <HomeIcon />
             </ListItemIcon>
-            <NavLink
-              to={frontendUrls.home}
-              activeClassName="border"
-            >
+            <NavLink to={frontendUrls.home} activeClassName="border">
               home
             </NavLink>
           </ListItem>
